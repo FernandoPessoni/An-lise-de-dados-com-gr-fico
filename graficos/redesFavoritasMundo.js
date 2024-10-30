bff7119b068f1db6a3e80a5e39e00426b9c36ee7
FernandoPessoni <104445764+FernandoPessoni@users.noreply.github.com>
2024-10-30 07:33:57 -0300

    ok

--- graficos/redesFavoritasMundo.js
+++ graficos/redesFavoritasMundo.js
@@ -40,11 +40,6 @@ async function redesFavoritasMundo() {
     },
   };
 
-
-  const grafico = document.createElement("div");
-  grafico.className = "grafico";
-  document.getElementById("graficos-container").appendChild(grafico);
-  Plotly.newPlot(grafico, data, layout);
 }
 
 redesFavoritasMundo();
